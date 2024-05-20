7864ca1783caf264cd7be985d395d7a0ab59028d
vishag002 <vvishag477@gmail.com>
2024-05-20 16:40:24 +0530

    model updated

--- lib/model/furniture.dart
+++ lib/model/furniture.dart
@@ -1,7 +1,17 @@
-class Furniture {
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class Furniture extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String imagrurl;
-  int price;
+
+  @HiveField(2)
+  double price;
+
+  @HiveField(3)
   int quantity;
 
   Furniture({
@@ -10,4 +20,24 @@ class Furniture {
     required this.price,
     this.quantity = 1,
   });
+
+  // Convert a Furniture object to a map.
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'imagrurl': imagrurl,
+      'price': price,
+      'quantity': quantity,
+    };
+  }
+
+  // Convert a map to a Furniture object.
+  factory Furniture.fromMap(Map<String, dynamic> map) {
+    return Furniture(
+      title: map['title'],
+      imagrurl: map['imagrurl'],
+      price: map['price'],
+      quantity: map['quantity'],
+    );
+  }
 }
